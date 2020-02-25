ff36561dffcd507650d51576f9445c158ebfd2ec
Bo Zheng <zhengbo@zhengbodembp.dyndns.rice.edu>
2020-02-25 15:29:25 -0600

    last file

---
+++ Pro2/recvfile.cpp
@@ -0,0 +1,315 @@
+//
+// Created by 郑博 on 2/25/20.
+//
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+/**************************************************/
+/* a few simple linked list functions             */
+/**************************************************/
+
+/* a buffer to read data */
+char *buf;
+int BUF_LEN = 65535;
+
+/* A linked list node data structure to maintain application
+   information related to a connected socket */
+struct node {
+    int socket;
+    struct sockaddr_in client_addr;
+    int pending_data; /* flag to indicate whether there is more data to send */
+    /* you will need to introduce some variables here to record
+       all the information regarding this socket.
+       e.g. what data needs to be sent next */
+    char* buffer;
+    unsigned short curSize;
+    unsigned short totalSize;
+    unsigned short offset;
+    unsigned short flag;
+    struct node *next;
+};
+
+/* remove the data structure associated with a connected socket
+   used when tearing down the connection */
+void dump(struct node *head, int socket) {
+    struct node *current, *temp;
+    current = head;
+
+    while (current->next) {
+        if (current->next->socket == socket) {
+            temp = current->next;
+            current->next = temp->next;
+            free(temp);
+            return;
+        } else {
+            current = current->next;
+        }
+    }
+}
+
+/* create the data structure associated with a connected socket */
+void add(struct node *head, int socket, struct sockaddr_in addr) {
+    struct node *new_node;
+
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->socket = socket;
+    new_node->client_addr = addr;
+    new_node->pending_data = 0;
+    new_node->next = head->next;
+    new_node->offset = 0;
+    new_node->buffer = (char *)malloc(BUF_LEN);
+    new_node->curSize = 0;
+    new_node->totalSize = 0;
+    new_node->flag = 0;
+    head->next = new_node;
+}
+
+void clear(struct node *current) {
+    memset(current->buffer, 0, BUF_LEN);
+    current->totalSize = 0;
+    current->offset = 0;
+    current->pending_data = 0;
+    current->curSize = 0;
+    current->flag = 0;
+    return;
+}
+/*****************************************/
+/* main program                          */
+/*****************************************/
+
+/* simple server, takes one parameter, the server port number */
+int main(int argc, char **argv) {
+
+    /* socket and option variables */
+    int sock, new_sock, max;
+    int optval = 1;
+
+    /* server socket address variables */
+    struct sockaddr_in sin, addr;
+    unsigned short server_port = atoi(argv[1]);
+
+
+    char* mode = argv[2];
+    char* root_directory = argv[3];
+
+    /* socket address variables for a connected client */
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+
+    /* maximum number of pending connection requests */
+    int BACKLOG = 5;
+
+    /* variables for select */
+    fd_set read_set, write_set;
+    struct timeval time_out;
+    int select_retval;
+
+    /* a silly message */
+    char *message = "Welcome! COMP/ELEC 429 Students!\n";
+
+    /* number of bytes sent/received */
+    int count;
+
+    /* numeric value received */
+    int num;
+
+    /* linked list for keeping track of connected sockets */
+    struct node head;
+    struct node *current, *next;
+
+
+
+    buf = (char *)malloc(BUF_LEN);
+
+    /* initialize dummy head node of linked list */
+    head.socket = -1;
+    head.next = 0;
+
+    /* create a server socket to listen for TCP connection requests */
+    if ((sock = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)              // server welcome socket
+    {
+        //perror ("opening TCP socket");
+        abort ();
+    }
+
+    /* set option so we can reuse the port number quickly after a restart */
+    if (setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof (optval)) <0)
+    {
+        //perror ("setting TCP socket option");
+        abort ();
+    }
+
+    /* fill in the address of the server socket */
+    memset (&sin, 0, sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons (server_port);
+
+    /* bind server socket to the address */
+    if (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)      // bind server ip port to socket
+    {
+        //perror("binding socket to address");
+        abort();
+    }
+
+    /* put the server socket in listen mode */
+    if (listen (sock, BACKLOG) < 0)                           // to listen status
+    {
+        //perror ("listen on socket failed");
+        abort();
+    }
+
+    /* now we keep waiting for incoming connections,
+       check for incoming data to receive,
+       check for ready socket to send more data */
+    while (1)
+    {
+        /* set up the file descriptor bit map that select should be watching */
+        FD_ZERO (&read_set); /* clear everything */               // file descriptor receive
+        FD_ZERO (&write_set); /* clear everything */              // file descriptor send out
+
+        FD_SET (sock, &read_set); /* put the listening socket in */
+        max = sock; /* initialize max */
+
+        /* put connected sockets into the read and write sets to monitor them */
+        for (current = head.next; current; current = current->next) {     // multiple connections
+            FD_SET(current->socket, &read_set);
+            if (current->pending_data) {                                 // read multiple times
+                /* there is data pending to be sent, monitor the socket
+                       in the write set so we know when it is ready to take more
+                       data */
+                FD_SET(current->socket, &write_set);
+            }
+
+            if (current->socket > max) {
+                /* update max if necessary */
+                max = current->socket;
+            }
+        }
+
+        time_out.tv_usec = 100000; /* 1-tenth of a second timeout */
+        time_out.tv_sec = 0;
+
+        /* invoke select, make sure to pass max+1 !!! */
+        select_retval = select(max+1, &read_set, &write_set, NULL, &time_out);    // cnt of variable socket
+        if (select_retval < 0)
+        {
+            //perror ("select failed");
+            abort ();
+        }
+
+        if (select_retval == 0)                                   // no var within timeout
+        {
+            /* no descriptor ready, timeout happened */
+            continue;
+        }
+
+        if (select_retval > 0) /* at least one file descriptor is ready */
+        {
+            if (FD_ISSET(sock, &read_set)) /* check the server socket */          // read var?
+            {
+                /* there is an incoming connection, try to accept it */
+                new_sock = accept (sock, (struct sockaddr *) &addr, &addr_len);       // new socket in server to connect r/w
+
+                if (new_sock < 0)
+                {
+                    //perror ("error accepting connection");
+                    abort ();
+                }
+
+                /* make the socket non-blocking so send and recv will
+                         return immediately if the socket is not ready.
+                         this is important to ensure the server does not get
+                         stuck when trying to send data to a socket that
+                         has too much data to send already.
+                    */
+                if (fcntl (new_sock, F_SETFL, O_NONBLOCK) < 0)
+                {
+                    //perror ("making socket non-blocking");
+                    abort ();
+                }
+
+                /* the connection is made, everything is ready */
+                /* let's see who's connecting to us */
+//                printf("Accepted connection. Client IP address is: %s\n",
+//                       inet_ntoa(addr.sin_addr));
+
+                /* remember this client connection in our linked list */
+                add(&head, new_sock, addr);
+            }
+
+            /* check other connected sockets, see if there is
+                   anything to read or some socket is ready to send
+                   more pending data */
+            for (current = head.next; current; current = next) {
+                next = current->next;
+                /* see if we can now do some previously unsuccessful writes */
+                if (FD_ISSET(current->socket, &write_set)) {
+                    /* the socket is now ready to take more data */
+                    /* the socket data structure should have information
+                           describing what data is supposed to be sent next.
+                       but here for simplicity, let's say we are just
+                           sending whatever is in the buffer buf
+                         */
+                    count = send(current->socket, current->buffer, current->totalSize, MSG_DONTWAIT);
+                    if (count < 0) {
+                        if (errno == EAGAIN) {
+                            /* we are trying to dump too much data down the socket,
+                               it cannot take more for the time being
+                               will have to go back to select and wait til select
+                               tells us the socket is ready for writing
+                            */
+                        } else {
+                            /* something else is wrong */
+                        }
+                    }
+                    /* note that it is important to check count for exactly
+                           how many bytes were actually sent even when there are
+                           no error. send() may send only a portion of the buffer
+                           to be sent.
+                    */
+                    clear(current);
+                }
+
+                if (FD_ISSET(current->socket, &read_set)) {
+                    /* we have data from a client */
+                    count = recv(current->socket, current->buffer + current->offset, BUF_LEN, 0);
+
+                    if (count <= 0) {
+                        /* something is wrong */
+                        if (count == 0) {
+                            //printf("Client closed connection. Client IP address is: %s\n", inet_ntoa(current->client_addr.sin_addr));
+                        } else {
+                            //perror("error receiving from a client");
+                        }
+
+                        /* connection is closed, clean up */
+                        close(current->socket);
+                        dump(&head, current->socket);
+                    } else {
+                        //Ping-Pong
+                        if(current->flag == 0) {
+                            unsigned short size_t = (unsigned short) ntohs(*(unsigned short *)(current->buffer));
+                            current->totalSize = size_t;
+                            current->flag = 1;
+                        }
+                        current->curSize += (unsigned short)count;
+                        current->offset += (unsigned short)count;
+                        if(current->totalSize == current->curSize) {
+                            current->pending_data = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Pro2/sendfile.cpp
@@ -0,0 +1,127 @@
+//
+// Created by 郑博 on 2/25/20.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+/* simple client, takes two parameters, the server domain name,
+   and the server port number */
+
+int main(int argc, char** argv) {
+
+    /* our client socket */
+    int sock, server_sock;
+
+    /* variables for identifying the server */
+    unsigned int server_addr;
+    struct sockaddr_in sin;
+    struct addrinfo *getaddrinfo_result, hints;
+
+    /* convert server domain name to IP address */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; /* indicates we want IPv4 */
+
+    if (getaddrinfo(argv[1], NULL, &hints, &getaddrinfo_result) == 0) {
+        server_addr = (unsigned int) ((struct sockaddr_in *) (getaddrinfo_result->ai_addr))->sin_addr.s_addr;
+        freeaddrinfo(getaddrinfo_result);
+    }
+
+    /* server port number */
+    unsigned short server_port = atoi (argv[2]);
+
+    char *buffer;
+    int size = 65535;
+    unsigned short send_size = (unsigned short)strtol(argv[3],NULL,10);
+    int cnt = atoi(argv[4]);
+    /* allocate a memory buffer in the heap */
+    /* putting a buffer on the stack like:
+
+           char buffer[500];
+
+       leaves the potential for
+       buffer overflow vulnerability */
+    buffer = (char *) malloc(size);
+    if (!buffer)
+    {
+        //perror("failed to allocated buffer");
+        abort();
+    }
+
+    /* create a socket */
+    if ((sock = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+        //perror ("opening TCP socket");
+        abort ();
+    }
+    /* fill in the server's address */
+    memset (&sin, 0, sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = server_addr;
+    sin.sin_port = htons(server_port);
+
+    /* connect to the server */
+    if (connect(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+        //perror("connect to server failed");
+        abort();
+    }
+
+    int i = 0;
+    float time_total = 0;
+    for(i = 0; i < cnt; i++) {
+        char data[send_size - 10];
+        memset(data, 'a', sizeof(data));
+        data[sizeof(data) - 1] = '\0';
+
+        unsigned short sendLen = sizeof(data) + 10;
+        char *sendbuffer;
+        sendbuffer = (char *) malloc(sendLen);
+
+        struct timeval start;
+        gettimeofday(&start, NULL);
+        time_t tv_sec = start.tv_sec;
+        suseconds_t tv_usec = start.tv_usec;
+        *(unsigned short *) (sendbuffer) = (unsigned short) htons(sendLen);
+        *(int *) (sendbuffer + 2) = (int) htonl((int)tv_sec);
+        *(int *) (sendbuffer + 6) = (int) htonl((int)tv_usec);
+        memcpy(sendbuffer + 10, data, sizeof(data));
+
+        send(sock, sendbuffer, sendLen, 0);
+
+        char *recbuffer;
+        recbuffer = (char *) malloc(sendLen);
+        unsigned short curSize = 0;
+        unsigned short offset = 0;
+        unsigned short recLen = 0;
+        while(curSize != sendLen) {
+            recLen = recv(sock, recbuffer + offset, sendLen, 0);
+            curSize += recLen;
+            offset += recLen;
+        }
+
+        // time latency
+        struct timeval end;
+        gettimeofday(&end, NULL);
+        float time_use=(end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) * 0.001;
+        printf("rtt:  %f\n", time_use);
+        if(time_use > 3){
+            continue;
+        }
+        time_total += time_use;
+        cnt--;
+    }
+
+    float time_avg = time_total / cnt;
+    //printf("cnt: %d\n", cnt);
+    //printf("time_avg: %f\n", time_avg);
+
+    return 0;
+}
