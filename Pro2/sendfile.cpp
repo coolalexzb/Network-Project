c4b2c4ec73912e5339f4d53968513b5416b6ae01
李瑶 <496365768@qq.com>
2020-03-02 22:43:12 -0600

    sendfile.cpp first version with millions of todos and bugs

--- Pro2/sendfile.cpp
+++ Pro2/sendfile.cpp
@@ -13,6 +13,7 @@
 #include <netdb.h>
 #include <sys/time.h>
 #include <fcntl.h>
+#include <PacketSendHandler.cpp>
 
 
 /* simple client, takes two parameters, the server domain name,
@@ -21,13 +22,31 @@
 char *buf;
 int BUF_LEN = 65535;
 
-int main(int argc, char** argv) {
+// ACK packet
+const int ACK_FLAG_POS = 0;
+const int ACK_SEQ_POS = 2;
+const int ACK_CHECKSUM_POS = 4;
+
+// send packet
+const int SEND_SEQ_POS = 0;
+const int SEND_CHECKSUM_POS = 2;
+const int SEND_LENGTH_POS = 4;
+
+// flag
+const int ACK_FINISH_FLAG = -1;
+const int ACK_INVALID_FLAG = -2;
+
+bool checkSum() {
+
+}
+
+int main(int argc, char **argv) {
 
 
-    char* recv_host = strtok(argv[2], ":");
+    char *recv_host = strtok(argv[2], ":");
     unsigned short recv_port = atoi(strtok(NULL, ":"));
-    char* subdir = strtok(argv[4], "/");
-    char* filename = strtok( NULL, "/");
+    char *subdir = strtok(argv[4], "/");
+    char *filename = strtok(NULL, "/");
 
     /* server socket address variables */
     struct sockaddr_in sin, sout, addr;
@@ -68,20 +87,22 @@ int main(int argc, char** argv) {
     sout.sin_addr.s_addr = inet_addr(recv_host);
     sout.sin_port = htons(recv_port);
 
-    buf = (char *)malloc(BUF_LEN);
+    buf = (char *) malloc(BUF_LEN);
 
+    // TODO: handle init?
+    PacketSendHandler handle = PacketSendHandler(filename);
 
     /* bind server socket to the address */
-    if (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)      // bind server ip port to socket
+    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0)      // bind server ip port to socket
     {
         perror("binding socket to address");
         abort();
     }
 
     /* put the server socket in listen mode */
-    if (listen (sock, BACKLOG) < 0)                           // to listen status
+    if (listen(sock, BACKLOG) < 0)                           // to listen status
     {
-        perror ("listen on socket failed");
+        perror("listen on socket failed");
         abort();
     }
 
@@ -90,22 +111,52 @@ int main(int argc, char** argv) {
      this is important to ensure the server does not get
      stuck when trying to send data to a socket that
      has too much data to send already.*/
-    if (fcntl (sock, F_SETFL, O_NONBLOCK) < 0)
-    {
-        perror ("making socket non-blocking");
-        abort ();
+    if (fcntl(sock, F_SETFL, O_NONBLOCK) < 0) {
+        perror("making socket non-blocking");
+        abort();
     }
 
-    while(1) {
+    timeval tv;
+    gettimeofday(&tv, nullptr);
+    time_t startTime = tv.tv_usec / 1000 + tv.tv_sec * 1000;
+    time_t checkTimeout = startTime;
+    while (1) {
+        // check timeout packet
+        gettimeofday(&tv, nullptr);
+        time_t currTime = tv.tv_usec / 1000 + tv.tv_sec * 1000;
+        if (currTime - checkTimeout > (time_out.tv_usec / 1000 + time_out.tv_sec * 1000)) {
+            packet resendPacket = handle.getNAckPacket(currTime);
+            gettimeofday(&tv, nullptr);
+            resendPacket.time = tv.tv_usec / 1000 + tv.tv_sec * 1000;
+            sendto(sock, resendPacket.data, resendPacket.len, 0, (sockaddr *) sout.sin_addr.s_addr, sizeof(sockaddr));
 
+            /*
+            vector<packet *> timeoutPackets = handle.getNAckPacket(currTime);
 
-        // TODO 1st send packet
+            // TODO ? auto exit after some retries (if the finish_ack is lost)
+            for (int i = 0; i < timeoutPackets.size(); i++) {
+                // TODO ???
+                packet tmpPacket = *(timeoutPackets)[i];
+                gettimeofday(&tv, nullptr);
+                tmpPacket.time = tv.tv_usec / 1000 + tv.tv_sec * 1000;
+                // TODO &???
+                sendto(sock, tmpPacket.data, tmpPacket.len, 0, (sockaddr *) sout.sin_addr.s_addr, sizeof(sockaddr));
+            }*/
 
+            checkTimeout = currTime;
+        }
 
-        // recv ACK
+        // send more packets
+        if (handle.isSlideWindowLeft() && handle.isOver() == false) {
+            packet newPacket = handle.newPacket();
+            gettimeofday(&tv, nullptr);
+            newPacket.time = tv.tv_usec / 1000 + tv.tv_sec * 1000;
+            sendto(sock, newPacket.data, newPacket.len, 0, (sockaddr *) sout.sin_addr.s_addr, sizeof(sockaddr));
+        }
 
 
         FD_ZERO (&read_set); /* clear everything */               // file descriptor receive
+        // ???
         FD_ZERO (&write_set); /* clear everything */              // file descriptor send out
 
         FD_SET (sock, &read_set); /* put the listening socket in */
@@ -115,10 +166,9 @@ int main(int argc, char** argv) {
 
         /* invoke select, make sure to pass max+1 !!! */
         select_retval = select(sock + 1, &read_set, &write_set, NULL, &time_out);    // cnt of variable socket
-        if (select_retval < 0)
-        {
-            perror ("select failed");
-            abort ();
+        if (select_retval < 0) {
+            perror("select failed");
+            abort();
         }
 
         if (select_retval == 0)                                   // no var within timeout
@@ -126,18 +176,37 @@ int main(int argc, char** argv) {
             /* no descriptor ready, timeout happened */
             continue;
         }
-
         if (select_retval > 0) /* at least one file descriptor is ready */
         {
             if (FD_ISSET(sock, &read_set))                  /* check the server socket */
             {
-                //TODO when recv ack packet
+                char *recBuff;
+                int recLen = recvfrom(sock, recBuff, BUF_LEN, 0, nullptr, nullptr);
+                if (checkSum()) {
+                    short ackFlag = (short)ntohs(*(short *) (recBuff + ACK_FLAG_POS));
+                    short ackSeq = (short)ntohs(*(short *) (recBuff + ACK_SEQ_POS));
+                    // receive a valid or invalid packet
+                    if (ackFlag > 0 || ackFlag == ACK_INVALID_FLAG) {
+                        handle.recv_ack(ackFlag, askSeq);
+                    }
+                    // receiving finished
+                    if (ackFlag == ACK_FINISH_FLAG) {
+                        cout << "FINISH PACKET" << endl;
+                        break;
+                    }
+
+                } else {
+                    cout << "wrong ack packet" << endl;
+                }
 
             }
 
         }
 
     }
+
+    gettimeofday(&tv, nullptr);
+    time_t endTime = tv.tv_usec / 1000 + tv.tv_sec * 1000;
 
     return 0;
 }
